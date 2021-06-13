4a39c132f8a814af16481ece69142feafd758f3d
RuthraVed <abhishek44dev@gmail.com>
2021-06-13 21:04:57 +0530

    Improved /price-plans/compare-all/
    After this update, the response will have a working 'bestPricePlanId'

--- src/controller/price_plan_comparator_controller.py
+++ src/controller/price_plan_comparator_controller.py
@@ -5,20 +5,24 @@ from service.price_plan_service import PricePlanService
 
 from .electricity_reading_controller import repository as readings_repository
 
+
 def compare(smart_meter_id):
     price_plan_service = PricePlanService(readings_repository)
     account_service = AccountService()
-    list_of_spend_against_price_plans = price_plan_service.get_list_of_spend_against_each_price_plan_for(smart_meter_id)
+    list_of_spend_against_price_plans = price_plan_service.get_list_of_spend_against_each_price_plan_for(
+        smart_meter_id)
 
     if len(list_of_spend_against_price_plans) < 1:
         abort(404)
     else:
         return {
-            "pricePlanId": account_service.get_price_plan(smart_meter_id),
+            "bestPricePlanId": next(iter(list_of_spend_against_price_plans[0])),
             "pricePlanComparisons": list_of_spend_against_price_plans
         }
 
+
 def recommend(smart_meter_id, limit=None):
     price_plan_service = PricePlanService(readings_repository)
-    list_of_spend_against_price_plans = price_plan_service.get_list_of_spend_against_each_price_plan_for(smart_meter_id, limit=limit)
+    list_of_spend_against_price_plans = price_plan_service.get_list_of_spend_against_each_price_plan_for(
+        smart_meter_id, limit=limit)
     return list_of_spend_against_price_plans
