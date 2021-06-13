8629f51f39aaabd84d1bd1a8f8e347972df82ce9
RuthraVed <abhishek44dev@gmail.com>
2021-06-13 16:40:27 +0530

    Changed random_time to get year 2021

--- src/generator/electricity_reading_generator.py
+++ src/generator/electricity_reading_generator.py
@@ -11,8 +11,9 @@ def random_int_between(min_val, max_val):
 def generate_electricity_readings(num):
     readings = []
     for i in range(num):
-        random_time = f"2001-{random_int_between(1,12)}-{random_int_between(1,28)}T{random_int_between(0,23)}:{random_int_between(0,59)}:{random_int_between(0,59)}"
+        random_time = f"2021-{random_int_between(1,12)}-{random_int_between(1,28)}T{random_int_between(0,23)}:{random_int_between(0,59)}:{random_int_between(0,59)}"
         random_reading = math.floor(random.random() * 1000)/1000
-        readings.append({"time": iso_format_to_unix_time(random_time), "reading": random_reading})
+        readings.append({"time": iso_format_to_unix_time(
+            random_time), "reading": random_reading})
 
     return readings
